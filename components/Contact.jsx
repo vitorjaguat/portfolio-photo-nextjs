c5007473e5785db24095b4022136f3b4cf8def0b
Vitor Butkus <vitorbutkus@gmail.com>
2023-02-07 19:18:47 -0300

    correct typo

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div className='max-w-[1240px] m-auto p-4 h-screen'>
       <h1 className='text-2xl font-bold text-center p-4'>
-        Let's work together
+        Let&apos;s work together
       </h1>
       <form className='max-w-[600px] m-auto'>
         <div className='grid grid-cols-2 gap-2'>
